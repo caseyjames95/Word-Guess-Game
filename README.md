53ad5dc94d29f4664933fda81298fabc6e73b11f
caseyjames95 <caseypurves@gmail.com>
2019-08-25 17:04:40 -0700

    Style and README
    Updated style layout and readme

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# Word-Guess-Game
+# Word Guess Game
+## Created by Casey Purves
+### [Github](https://github.com/caseyjames95) | [LinkedIn](https://www.linkedin.com/in/caseypurves/)
+Made using HTML, CSS and JavaScript.
+
+
+
+### html.index
+ - Page layout
+ - linking sources together (stylesheet, javascript)
+ - Background styling
+
+### game.js
+ - Contains HTML DOM
+ - Game functions and operations
+
+### style.css
+ - Personal layout of page
+
+### backdrop.css
+ - Jack Phillippi [CodePen Link](https://codepen.io/jackphilippi/pen/LjezQX) 
+ - all backdrop and header configuration files
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,6 +3,7 @@
 let winCount = 0
 let lossCount = 0
 let guessCount = 0
+let lguess = 1 + guessCount
 const win = true
 const loss = false
 const maxCount = 9
@@ -17,22 +18,29 @@ document.onkeyup = function (lguess) {
 }
 
 
-
 // Picks word or letter
+// function ranWord () {
+//     const randomItem = wordSel[Math.floor(Math.random()*wordSel.length)]
+//     console.log(word);
+
+//     return randomItem
+// }
+
 const randomItem = wordSel[Math.floor(Math.random()*wordSel.length)]
 document.getElementById(`gameDisplay`).innerHTML = randomItem
 
 // Score Board
 document.getElementById('guess').innerHTML = `
-<h3>Guesses = ${guessCount}/9
+<h3>Guesses = ${guessCount}/9</h3>
 `
 document.getElementById('win').innerHTML = `
-<h3>Wins = ${winCount}
+<h3>Wins = ${winCount}</h3>
 `
 document.getElementById('loss').innerHTML = `
-<h3>Losses = ${lossCount}
+<h3>Losses = ${lossCount}</h3>
 `
 
+// Objects
 
 // Game restart
 if (win) {
